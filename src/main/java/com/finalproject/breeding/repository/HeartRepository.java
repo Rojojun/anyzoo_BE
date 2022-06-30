e1398a03763fa6ceb3b2e3524e8a1be115860d79
dohunkwon <ehgns24@naver.com>
2022-06-30 23:58:36 +0900

    커뮤니티 다날림;;

--- src/main/java/com/finalproject/breeding/repository/HeartRepository.java
+++
@@ -1,7 +0,0 @@
-package com.finalproject.breeding.repository;
-
-import com.finalproject.breeding.model.Heart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HeartRepository extends JpaRepository<Heart, Long> {
-}
