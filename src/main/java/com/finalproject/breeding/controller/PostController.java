51a54fa695cc6275dc5740561020e09793f1b1d7
Rojojun <72805486+Rojojun@users.noreply.github.com>
2022-07-02 02:29:05 +0900

    [etc] 커밋 테스트

--- src/main/java/com/finalproject/breeding/controller/PostController.java
+++ src/main/java/com/finalproject/breeding/controller/PostController.java
@@ -1,9 +1,15 @@
 package com.finalproject.breeding.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class PostController {
+
+    // 자랑하게 (가제) 작성
+    @PostMapping("/api/post")
+    public
+
 }
---
+++ src/main/java/com/finalproject/breeding/dto/BoardMainRequestDto.java
@@ -0,0 +1,5 @@
+package com.finalproject.breeding.dto;
+
+public class BoardMainRequestDto {
+    private String content;
+}
