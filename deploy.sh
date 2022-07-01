e8dade4e080fa1ffec04ea5c45e23f78321b6557
Rojojun <72805486+Rojojun@users.noreply.github.com>
2022-07-01 15:44:40 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,20 @@
-version: 0.0
-os: linux
+REPOSITORY=/home/ec2-user/app
+cd $REPOSITORY
 
-files:
-  - source: /
-    destination: /home/ec2-user/app # 인스턴스에서 파일이 저장될 위치
-hooks:
-  AfterInstall:
-    - location: deploy.sh
-      timeout: 60
-      runas: root
+APP_NAME=breeding #1
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ] #2
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  sudo kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> $JAR_PATH 배포" #3
+nohup java -jar /home/ec2-user/app/build/libs/curriculum-1.0.jar --spring.config.location=/home/ec2-user/application.yml > /dev/null 2> /dev/null < /dev/null &
