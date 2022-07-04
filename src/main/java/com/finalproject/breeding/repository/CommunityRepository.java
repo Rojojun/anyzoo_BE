3c88781058bb7ebbc81e36183ec9a442bff4caa1
dohunkwon <ehgns24@naver.com>
2022-07-04 11:07:19 +0900

    no message

--- src/main/java/com/finalproject/breeding/repository/CommunityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.finalproject.breeding.repository;
-
-import com.finalproject.breeding.model.board.Community;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommunityRepository extends JpaRepository<Community, Long> {
-}
