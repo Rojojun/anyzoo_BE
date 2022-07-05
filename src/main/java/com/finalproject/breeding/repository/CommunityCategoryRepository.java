50aa5ecc9c6ee9a05d0d373d8d7468d73015c789
dohunkwon <ehgns24@naver.com>
2022-07-05 11:28:41 +0900

    pull

--- src/main/java/com/finalproject/breeding/repository/CommunityCategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.finalproject.breeding.repository;
-
-import com.finalproject.breeding.model.Comment;
-import com.finalproject.breeding.model.category.CommunityCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommunityCategoryRepository extends JpaRepository<CommunityCategory, Long> {
-}
