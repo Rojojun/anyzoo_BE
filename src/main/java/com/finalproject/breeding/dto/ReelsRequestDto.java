b4bd34c5c255055f79b890a6f68def31b884f4ba
Rojojun <zoloman316@gmail.com>
2022-07-07 16:54:04 +0900

    [etc] 도훈 댓글 통합 into 최근 릴리즈 (건, 지훈, 호준)

---
+++ src/main/java/com/finalproject/breeding/dto/ReelsRequestDto.java
@@ -0,0 +1,12 @@
+package com.finalproject.breeding.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReelsRequestDto {
+
+    String video;
+    String titleImg;
+}
--- src/main/java/com/finalproject/breeding/repository/CommentRepository.java
+++ src/main/java/com/finalproject/breeding/repository/CommentRepository.java
@@ -3,5 +3,9 @@ package com.finalproject.breeding.repository;
 import com.finalproject.breeding.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment>findAllByBoardMain_Id(Long boardMainId);
+
 }
