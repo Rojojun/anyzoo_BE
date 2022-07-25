cec51cca5b255d349d30fb9bfc5ad5e6ea5ca9e4
dohunkwon <ehgns24@naver.com>
2022-07-25 21:07:08 +0900

    대댓글 수 불러오기

--- src/main/java/com/finalproject/breeding/etc/repository/ReplyRepository.java
+++ src/main/java/com/finalproject/breeding/etc/repository/ReplyRepository.java
@@ -5,10 +5,12 @@ import com.finalproject.breeding.etc.model.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
-    List<Reply> findByCommentId(Long CommentId);
+    List<Reply> findByCommentId(Long commentId);
     List<ReplyMapping> findAllByCommentId(Long commentId);
+
 
 
 }
